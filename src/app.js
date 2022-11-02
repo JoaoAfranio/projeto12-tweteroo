131b5904a8f0f4dc5f75aec9ca4e9f29748c6385
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-11-01 22:12:05 -0300

    creating singup route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 import express from "express";
 const PORT = 5000;
 const app = express();
+app.use(express.json());
+
+const users = [];
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
+});
+
+app.post("/sign-up", (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.send("ok");
 });
